6fc773df5e8aeb81ea501f79c8d4134c4e70d809
Yol-24 <54098157+Yol-24@users.noreply.github.com>
2019-11-27 21:40:57 +0800

    added save to db code

--- server.js
+++ server.js
@@ -1,35 +1,65 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
 const multer = require('multer');
+const Model = require('./model/image')
 const path = require('path');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use('/static', express.static(path.join(__dirname, 'uploads')))
+app.use('/files', express.static(path.join(__dirname, 'uploads')))
+
+
+const PORT = 5000
+var imgUrl = `http://localhost:${PORT}/files/`
+
+//Connect to DB
+const mongoose = require('mongoose')
+const mongoURI = "mongodb://localhost:27017/Images"
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}, (err, data) => {
+    if (err) {
+        console.log("error : " + err);
+    } else {
+        console.log("database is connected!");
+    }
+});
+
+var ImageSchema = mongoose.Schema({
+    name: String,
+    src: String
+});
+
+// compile schema to model
+var Image = mongoose.model('Image', ImageSchema, 'images'); //images is the collection
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, 'uploads')
     },
-    filename: function (req, file, cb) {
-        var filename = "uploads_" + file.originalname
+    filename: function(req, file, cb) {
+        var filename = `uploads_${Math.round(+new Date()/1000)}_${file.originalname}`
         cb(null, filename)
     }
 })
 
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage, limits: { fileSize: 1000000000 } })
 
-// function to save image url  to mongodb
-let store = (filename) => {
-    //your servername + filename
-    var imgUrl = 'http://localhost:3000/static/uploads/' + filename; //save this to db  
-}
 
 // note 'img' in upload is the key you use in FormData in frontend
 //e.g : var data =  new FormData()
 // data.append('img' ,uploadedFiles)
 
-app.post('/uploadMultiple', upload.array('img', 10), (req, res, next) => {
+app.post('/uploadMultiple', upload.array('img'), (req, res, next) => {
     const imgs = req.files
     if (!imgs) {
         const error = new Error('Please upload a file')
@@ -37,11 +67,19 @@ app.post('/uploadMultiple', upload.array('img', 10), (req, res, next) => {
         return next(error)
     } else {
         imgs.map(img => {
-            store(img.filename)
+            let src = `${imgUrl}${img.filename}`; //save this to db  
+            var new_img = new Image({ name: img.filename, src: src });
+            // save model to database
+            new_img.save(function(err, imgSaved) {
+                if (err) return console.error(err);
+                console.log(imgSaved.name + " saved to images collection.");
+            });
+            img.src = `http://localhost:${PORT}/static/uploads/${img.filename}`
         })
-        res.send("success")
+        res.send(imgs)
     }
 })
+
 
 app.post('/uploadSingle', upload.single('img'), (req, res, next) => {
     const img = req.file
@@ -49,15 +87,12 @@ app.post('/uploadSingle', upload.single('img'), (req, res, next) => {
         const error = new Error('Please select a file')
         error.httpStatusCode = 400
         return next(error)
-    }
-    else {
+    } else {
         store(img.filename)
         res.send("success")
     }
 })
 
-app.listen(3000, () => {
-    console.log(`server running at ${3000}`);
+app.listen(PORT, () => {
+    console.log(`server running at ${PORT}`);
 })
-
-
